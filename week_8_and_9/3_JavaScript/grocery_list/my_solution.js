0135fb241533f5dd67b8d7cb8d69b49e526ac5ce
Greg <greg.knudsen09@gmail.com>
2014-05-11 10:54:59 -0400

    Submit grocery list

--- week_8_and_9/3_JavaScript/grocery_list/my_solution.js
+++ week_8_and_9/3_JavaScript/grocery_list/my_solution.js
@@ -1,15 +1,41 @@
 // U3.W8-9: 
 
 
-// I worked on this challenge [by myself, with: ].
+// I worked on this challenge by myself.
 
 // 2. Pseudocode
-
+/*
+	
+create object 'groceries' with a few grocery items also as objects,
+	where the key is the item, and the value is the amount needed
+write function 'addToList' that adds items to the grocery list
+write function 'remove' that takes away item
+write function 'display' that displays list
+	
+*/
 
 
 // 3. Initial Solution
 
+var groceries = {
+	apple: 	3,
+	orange: 2,
+	banana: 5
+};
 
+var addToList = function(item, amount) {
+	groceries[item] = amount;
+};
+
+var remove = function(item) {
+	groceries.delete(item);
+};
+
+var display = funtion() {
+	for(var i = 0; i < groceries.length; i++){
+		console.log("You have " + groceries[amount] +  groceries[item] + "left to buy!")
+	}
+}
 
 
 // 4. Refactored Solution
@@ -21,7 +47,9 @@
 
 // 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
-
+var assert = function(object){
+	
+}
 
 
 
