97423320e48c5eaa91b75f32cc50a1aaec414622
Greg <greg.knudsen09@gmail.com>
2014-05-11 21:59:19 -0400

    submit

--- week_8_and_9/4_Ruby/rectangle_class/my_solution.rb
+++ week_8_and_9/4_Ruby/rectangle_class/my_solution.rb
@@ -1,10 +1,22 @@
 # U3.W8-9: Implement a Rectangle Class
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
+=begin
 
+create 4 instance methods - #area, #perimeter, #diagonal, #square?
+for #area
+	return value from @height * @width
+for #perimeter
+	return value from (@height*2)	plus (@width*2)
+for #diagonal
+	return square root of @height**2 + @width**2
+for #square
+	return true if @height = @width
+
+=end
 
 
 # 3. Initial Solution
@@ -16,7 +28,23 @@ class Rectangle
     @height = height
   end
 
-  def ==(other)
+  def area
+  	@height * @width
+  end
+
+  def perimeter
+  	(@height * 2) + (@width * 2)
+  end
+
+  def diagonal
+  	Math.sqrt(@width**2 + @height**2)
+  end
+
+  def square?
+  	true if @height == @width
+  end
+
+	def ==(other)
     (other.width  == self.width && other.height == self.height ) ||
     (other.height == self.width && other.width  == self.height )
   end
@@ -27,6 +55,8 @@ end
 
 # 4. Refactored Solution
 
+#I'm sure I could refactor this, but I just don't have time.
+
 
 
 
@@ -34,6 +64,11 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+def assert
+	puts "ASSERTION FAILED" unless yield
+end
+
+assert {  }
 
 
 
