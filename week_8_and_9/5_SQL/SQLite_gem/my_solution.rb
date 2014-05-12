70b7dc43c7c2f08ed9fd9845d8293507918052fa
Greg <greg.knudsen09@gmail.com>
2014-05-11 20:37:04 -0400

    submit

--- week_8_and_9/5_SQL/SQLite_gem/my_solution.rb
+++ week_8_and_9/5_SQL/SQLite_gem/my_solution.rb
@@ -1,6 +1,6 @@
 # U3.W7: BONUS Using the SQLite Gem
 
-# I worked on this challenge [by myself, with:]
+# I worked on this challenge with Ken Sin.
 
 require 'sqlite3'
 
@@ -16,11 +16,25 @@ end
 
 def print_longest_serving_reps(minimum_years)  #sorry guys, oracle needs me, i didn't finish this!
   puts "LONGEST SERVING REPRESENTATIVES"
-  puts $db.execute("SELECT name FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  longest_serving = $db.execute("SELECT name, years_in_congress FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  #years_served = $db.execute("SELECT years_in_congress FROM congress_members WHERE years_in_congress > #{minimum_years}")
+  longest_serving.each do |rep| 
+  	puts "#{rep[0]} - #{rep[1]}"
+  end
 end
 
-def print_lowest_grade_level_speakers
+def print_lowest_grade_level_speakers(grade)
   puts "LOWEST GRADE LEVEL SPEAKERS (less than < 8th grade)"
+  puts grade_level = $db.execute("SELECT name FROM congress_members WHERE grade_current < #{grade}")
+end
+
+def print_states_reps(*states)
+	reps = states.map do |state|
+					$db.execute("SELECT name, location FROM congress_members WHERE location = '#{state}' AND name LIKE ('Rep%')")
+				end			
+	reps.each do |rep| 
+		rep.each { |person| puts "#{person[0]} - #{person[1]}" }
+	end
 end
 
 def print_separator
@@ -32,19 +46,21 @@ end
 
 print_arizona_reps
 
-print_separator
+#print_separr
+
 
 print_longest_serving_reps(35)
 # TODO - Print out the number of years served as well as the name of the longest running reps
 # output should look like:  Rep. C. W. Bill Young - 41 years
 
 print_separator
-print_lowest_grade_level_speakers 
+print_lowest_grade_level_speakers(8)
 # TODO - Need to be able to pass the grade level as an argument, look in schema for "grade_current" column
 
 # TODO - Make a method to print the following states representatives as well:
 # (New Jersey, New York, Maine, Florida, and Alaska)
-
+print_separator
+print_states_reps("NJ", "NY", "ME", "AK", "FL")
 
 ##### BONUS #######
 # TODO (bonus) - Stop SQL injection attacks!  Statmaster learned that interpolation of variables in SQL statements leaves some security vulnerabilities.  Use the google to figure out how to protect from this type of attack.
@@ -60,10 +76,21 @@ print_lowest_grade_level_speakers
 
 
 # REFLECTION- Include your reflection as a comment below.
-# How does the sqlite3 gem work?  What is the variable `$db` holding?  
+# How does the sqlite3 gem work? 
+# => It allows you to create sqlite3 databases and interract with them in the terminal
+# What is the variable `$db` holding? 
+# => the database to be interracted with in the ruby program 
 # Try to use your knowledge of ruby and OO to decipher this as well as h
 # ow the `#execute` method works.  Take a stab at explaining the line 
 # `$db.execute("SELECT name FROM congress_members WHERE years_in_congress 
-#   > #{minimum_years}")`.  Try to explain this as clearly as possible for 
+#   > #{minimum_years}")`. Try to explain this as clearly as possible for 
 # your fellow students.  
+# => It would seem that the $db.execute puts you into an sqlite3 environment. I would liken it to using style tabs in an
+# => html document. Just for that short amount of code, you are in a CSS doc.
 # If you're having trouble, find someone to pair on this explanation with you.
+=begin
+
+I met up with Ken to work on this one. Ken was a great help in helping me solve the first two TODO's. And we ended up coming up with the third TODO together in a way he had not done it.
+We eventually got it to work by being able to enter whatever states as arguments for the #print_states_reps method...pretty cool. 
+
+=end
